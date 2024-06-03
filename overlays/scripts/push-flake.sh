1b38c92df1d8ebb4b18262ae1cde685ffa7969ba
Niklas Gollenstede <gollenstede@ibr.cs.tu-bs.de>
2024-06-03 16:48:39 +0200

    add "--register" flag to "with" command

--- overlays/scripts/push-flake.sh
+++ overlays/scripts/push-flake.sh
@@ -5,9 +5,10 @@ description="Pushes a Nix »flake« (default: .) and its local inputs to a remot
 argvDesc='target [flake]'
 declare -g -A allowedArgs=(
     [--types=indirect,path]="The types of input flakes that will be pushed. Should include all types that the remote won't be able to pull itself."
+    [--register[=<name>]]="Register the flake in the target's (user) flake registry as »name«, or the name of the flake directory as inferred from »\$flake«. The »\$target« has to be an SSH [user@]hostname/IP. Specifically, this (re-)places an entry »from:{type:indirect,id:\$name},to:<pushed>« in »\$target:.config/nix/registry.json«."
 )
 details="
-»target« can be a plain (SSH) hostname/IP or any valid Nix store URL.
+»target« can be a plain (SSH) [user@]hostname/IP or any valid Nix store URL.
 »flake« should be a local filesystem path. It can also be a »git+file://«-URL, but in that case »dir« is the only supported parameter. Defaults to ».«.
 "
 
@@ -18,6 +19,8 @@ exitCode=2 generic-arg-verify || exit
 targetStore=${argv[0]:?}
 if [[ $targetStore != *://* ]] ; then
     targetStore='ssh://'$targetStore
+elif [[ ${args[register]:-} ]] ; then
+    echo "--register can't be used with explicit nix store URL as »target«" ; exit 1
 fi
 
 flakeSpec=${argv[1]:-.}
@@ -42,5 +45,25 @@ in builtins.concatStringsSep " " ([ flake.outPath ] ++ (map (name: (
 ))); }' --argstr flakeLock "$flakeLock" --argstr flakeSpec "$flakeSpec" --argstr localTypes "${args[types]:-indirect,path}" --raw -- result ) ) || exit
 : ${storePaths[0]:?}
 
-PATH=@{pkgs.openssh}/bin:@{pkgs.hostname-debian}/bin:@{pkgs.gnugrep}/bin:$PATH @{pkgs.nix}/bin/nix --extra-experimental-features 'nix-command flakes' copy --to "$targetStore" ${storePaths[@]} || exit ; echo ${storePaths[0]}
+if [[ ${args[register]:-} ]] ; then storePaths+=( @{pkgs.jq} ) ; fi
+
+PATH=@{pkgs.openssh}/bin:@{pkgs.hostname-debian}/bin:@{pkgs.gnugrep}/bin:$PATH @{pkgs.nix}/bin/nix --extra-experimental-features 'nix-command flakes' copy --to "$targetStore" ${storePaths[@]} || exit
 # ¿¿Why does something there call »hostname -I«, which is apparently only available in the debian version of hostname??
+echo ${storePaths[0]}
+
+if [[ ${args[register]:-} ]] ; then
+    if [[ ${args[register]:-} == 1 ]] ; then args[register]=$( basename "$( dirname "$flakeLock" )" ) ; fi
+
+    ssh -q -t "${argv[0]:?}" -- "$( function remote { set -o pipefail -u
+        jq=$1 ; shift ; path=$1 ; shift ; name=$1 ; shift ; args=( --arg path "$path" --arg name "$name" )
+        if [[ -e .config/nix/registry.json ]] ; then reg=$(<.config/nix/registry.json) ; else reg='{ "version": 2, "flakes": [ ] }' ; fi
+        isVer=$( <<<$reg $jq .version ) ; if [[ $isVer != 2 ]] ; then echo "Unexpected flake registry's version ${isVer:-(empty)}" ; exit 2 ; fi
+
+        if [[ $( <<<$reg $jq "${args[@]}" '.flakes[] | select(.from.type == "indirect" and .from.id == $name)' ) ]] ; then
+            reg=$( <<<$reg $jq "${args[@]}" '( .flakes[] | select(.from.type == "indirect" and .from.id == $name) ).to |= { type: "path", path: $path, }' )
+        else
+            reg=$( <<<$reg $jq "${args[@]}" '.flakes += [ { from: { type: "indirect", id: $name }, to: { type: "path", path: $path, } } ]' )
+        fi
+        mkdir -p .config/nix/ ; <<<$reg cat >.config/nix/registry.json
+    } ; declare -f remote ) ; remote $( printf ' %q' @{pkgs.jq}/bin/jq "${storePaths[0]}" "${args[register]}" )" || exit
+fi
--- overlays/scripts/update-host.sh
+++ overlays/scripts/update-host.sh
@@ -13,28 +13,28 @@ drvPath=$( nix eval --raw .#nixosConfigurations."$hostname".config.system.build.
 PATH=@{pkgs.openssh}/bin:@{pkgs.hostname-debian}/bin:@{pkgs.gnugrep}/bin:$PATH @{pkgs.nix}/bin/nix --extra-experimental-features 'nix-command flakes' copy --to ssh://"$targetHost" --derivation "$drvPath^*" || exit
 
 ssh -q -t "$targetHost" -- "$( function remote { set -o pipefail -u
-	drvPath=$1 ; shift ; predicate=$1 ; shift
-	function version-gr-eq { printf '%s\n%s' "$1" "$2" | LC_ALL=C sort -C -V -r ; }
+    drvPath=$1 ; shift ; predicate=$1 ; shift
+    function version-gr-eq { printf '%s\n%s' "$1" "$2" | LC_ALL=C sort -C -V -r ; }
     output= ; if version-gr-eq "$( nix --version | grep -Poe '\d+.*' )" '2.14' ; then output='^out' ; fi
-	systemPath=$( nix build --no-link --print-out-paths "$drvPath"$output "$@" ) || exit
-	sudo= ; if [[ ${UID:-0} != 0 ]] ; then sudo=sudo ; fi
-	$sudo nix-env -p /nix/var/nix/profiles/system --set "$systemPath" || exit
-	$sudo "$systemPath"/bin/switch-to-configuration "$predicate" || exit
+    systemPath=$( nix build --no-link --print-out-paths "$drvPath"$output "$@" ) || exit
+    sudo= ; if [[ ${UID:-0} != 0 ]] ; then sudo=sudo ; fi
+    $sudo nix-env -p /nix/var/nix/profiles/system --set "$systemPath" || exit
+    $sudo "$systemPath"/bin/switch-to-configuration "$predicate" || exit
 } ; declare -f remote ) ; remote $( printf ' %q' "$drvPath" "$predicate" "$@" )" || exit
 
 exit
 
 while true ; do
-	if [[ -e flake.nix ]] ; then flakePath=$PWD ; break ; fi
-	cd .. ; if [[ $PWD == / ]] ; then echo 'Unable to locate flake.nix in (parent of) CWD' >&2 ; exit 2 ; fi
+    if [[ -e flake.nix ]] ; then flakePath=$PWD ; break ; fi
+    cd .. ; if [[ $PWD == / ]] ; then echo 'Unable to locate flake.nix in (parent of) CWD' >&2 ; exit 2 ; fi
 done
 while true ; do
-	if [[ -e .git/config ]] ; then repoPath=$PWD ; break ; fi
-	cd .. ; if [[ $PWD == / ]] ; then echo 'Unable to locate .git/config in (parent of) CWD' >&2 ; exit 2 ; fi
+    if [[ -e .git/config ]] ; then repoPath=$PWD ; break ; fi
+    cd .. ; if [[ $PWD == / ]] ; then echo 'Unable to locate .git/config in (parent of) CWD' >&2 ; exit 2 ; fi
 done
 
 flakeArgs= ; if [[ $repoPath != "$flakePath" ]] ; then
-	flakeArgs=?dir="${flakePath/$repoPath'/'/}"
+    flakeArgs=?dir="${flakePath/$repoPath'/'/}"
 fi
 
 storePath=$( @{pkgs.push-flake!getExe} ssh://"$targetHost" git+file://"$repoPath""$flakeArgs" )
